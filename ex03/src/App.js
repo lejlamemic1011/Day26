6ae448352061060ca5196a0ef171c2c2741c3bdc
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-04-02 11:24:41 +0200

    Add files via upload

---
+++ ex03/src/App.js
@@ -0,0 +1,12 @@
+import GetRequestErrorHandling from './GetRequestErrorHandling';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <GetRequestErrorHandling />
+    </div>
+  );
+}
+
+export default App;
