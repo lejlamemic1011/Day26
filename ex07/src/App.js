588fbd3d1cf05234e63071158b1a1cb53f3228aa
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-04-02 21:03:08 +0200

    Add files via upload

---
+++ ex07/src/App.js
@@ -0,0 +1,12 @@
+import PostRequestErrorHandling from './PostRequestErrorHandling';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PostRequestErrorHandling />
+    </div>
+  );
+}
+
+export default App;
