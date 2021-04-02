1cacaada57022f65363b695214acebd494238f01
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-04-02 21:02:22 +0200

    Add files via upload

---
+++ ex06/src/App.js
@@ -0,0 +1,12 @@
+import PostRequestAsyncAwait from './PostRequestAsyncAwait';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <PostRequestAsyncAwait />
+    </div>
+  );
+}
+
+export default App;
