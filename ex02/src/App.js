7700bb27cfb97f687a08e5cc76872851e0cc044f
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-04-02 11:24:12 +0200

    Add files via upload

---
+++ ex02/src/App.js
@@ -0,0 +1,12 @@
+import GetRequestAsyncAwait from './GetRequestAsyncAwait';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequestAsyncAwait />
+    </div>
+  );
+}
+
+export default App;
