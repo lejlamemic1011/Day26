4c404950bac238df7810a339b8ca05256cccbec5
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-04-02 14:42:51 +0200

    Add files via upload

---
+++ ex04/src/App.js
@@ -0,0 +1,12 @@
+import GetRequestSetHeaders from './GetRequestSetHeaders';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <GetRequestSetHeaders />
+    </div>
+  );
+}
+
+export default App;
