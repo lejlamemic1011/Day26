dbc5a24cd83f6494179d0d1d08b2eb5cbfd74b8d
lejlamemic1011 <80248070+lejlamemic1011@users.noreply.github.com>
2021-04-02 21:46:51 +0200

    Add files via upload

--- ex05/src/PostRequest.js
+++ ex05/src/PostRequest.js
@@ -9,7 +9,6 @@ class PostRequest extends React.Component {
 
     componentDidMount() {
         const article = { title: 'React POST Request Example' };
-
         axios.post('https://reqres.in/api/articles', article)
             .then(response => this.setState({ articleId: response.data.id }));
     }
